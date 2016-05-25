675f48f7b593cc2db8dffc48bd5509d6ed18b8f5
samelie <sam@stinkdigital.com>
2016-05-25 21:36:27 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#tiled montage of images
+
+
+node montage-images.js --d <folder_of_images> --r <resize_image_to_height> --fps --h <target_motageHeight>
+
+![alt-text](https://s3-eu-west-1.amazonaws.com/chromegno.me/_OUT.jpg)
---
+++ montage-image.js
@@ -0,0 +1,72 @@
+'use strict';
+
+require('shelljs/global')
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
+const rm  = require('rimraf')
+const yargs = require('yargs')
+const readdir = require('readdir')
+
+const argv = yargs.argv
+const ASPECT = 1.779166667
+//^^^^^
+/*16x9 images */
+
+let DIR = argv.d || ''
+let FORMAT = argv.format || 'jpg'
+let RESIZE = argv.r || 64
+let FPS = Math.round(25 / (argv.fps || 2))
+let PAPERHEIGHT = argv.h || 4961 //A3
+
+
+
+function selectImages(allPaths){
+	let _newPaths = []
+	allPaths.forEach((p,i)=>{
+		if(i % FPS === 0){
+			_newPaths.push(p)
+		}
+	})
+
+	identify(_newPaths[0])
+	resizeAllImages(_newPaths)
+}
+
+function identify(p){
+	let o = exec(`identify ${p} -format "%w x %h"`).stdout
+	console.log(o);
+}
+
+function resizeAllImages(paths){
+	let _outDir = path.join(path.parse(paths[0]).dir, '_out')
+	let _newImages = []
+	rm(_outDir, ()=>{
+		fs.mkdirSync(_outDir)
+		paths.forEach((p,i) =>{
+			let pr = path.parse(p)
+			let b = pr.name + '_r'
+			let o = path.join(_outDir, b) + pr.ext
+			_newImages.push(o)
+			exec(`convert ${p} -resize x${RESIZE} ${o}`)
+			console.log(i, paths.length-1);
+			if(i === paths.length-1){
+				_montage(_outDir)
+			}
+		})
+	})
+}
+
+function _montage(outDir){
+	let _w = RESIZE * ASPECT
+	let _row = Math.ceil(PAPERHEIGHT / RESIZE)
+	cd(outDir)
+	exec(`montage *.${FORMAT} -tile x${_row} -geometry ${_w}x${RESIZE} _OUT.jpg`)
+}
+
+selectImages
+
+let _images = readdir.readSync(path.join(__dirname, DIR) , [`*.${FORMAT}`], readdir.ABSOLUTE_PATHS );
+
+selectImages(_images)
+
